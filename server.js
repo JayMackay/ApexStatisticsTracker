3b394e0a64d24e177019bc5b70a6fde7fc40d2d5
JayMackay <jared.mackay1994@live.com>
2019-12-17 22:43:03 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//Load configuration file
+dotenv.config({ path: './config.env' })
+
+const app = express();
+
+app.get('/api/v1/profile/:platform/:gamertag',(req, res) => {
+    console.log(req.params.platform, req.params.gamertag);
+    res.send('Hello');
+});
+
+const port=process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server running on ${process.env.NODE_ENV} mode on port ${port}`);
+});
